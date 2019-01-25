15a08cb43a70a13abdcbc582ee2802904ff4a969
emmiejw <emilyprice84@outlook.com>
2019-01-25 16:23:27 +0000

    home page  created and diplaying jobs

--- blog/apps.py
+++ blog/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class blogConfig(AppConfig):
+class BlogConfig(AppConfig):
     name = 'blog'
--- blog/models.py
+++ blog/models.py
@@ -1,2 +1,7 @@
 from django.db import models
 
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
